e1c46858d6e085dbf67d2726bc73e80ab8399a71
HongDaYeon <ekdus12037406@gmail.com>
2021-10-04 10:52:35 +0900

    20211004_OpenCV

---
+++ study/08.image_color_brightness.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+# src = cv2.imread("../img/psycho_but_okay.jpg", cv2.IMREAD_GRAYSCALE)
+src = cv2.imread("../img/psycho_but_okay.jpg", cv2.IMREAD_COLOR)
+
+# Blue색에만 더해진다
+add_dst = cv2.add(src, 100)
+sub_dst = cv2.subtract(src, 100)
+
+cv2.imshow('src', src)
+cv2.imshow('add_dst', add_dst)
+cv2.imshow('sub_dst', sub_dst)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
--- study/12.binary_image.py
+++ study/12.binary_image.py
@@ -1,15 +1,15 @@
 import cv2
 
-# color 이미지를 binary로 하면 각 rab 채널별로 binary한 후 합쳐진 것
-# 그래서 color 이미지는 binary하지 않는다
-# src = cv2.imread("../img/swan.jpg)
+# Color이미지를 binary로 하면 각rgb채널별로 binary한 후 합쳐진 것이다
+# 그래서 Color 이미지는 binary를 하지 않는다
+# src = cv2.imread("../img/swan.jpg")
 
-src = cv2.imread('../img/swan.jpg')
+src = cv2.imread("../img/swan.jpg", cv2.IMREAD_GRAYSCALE)
 
-_, binary= cv2.threshold(src, 127,255,cv2.THRESH_BINARY)
+_, binary = cv2.threshold(src, 127, 255, cv2.THRESH_BINARY)
 
-cv2.namedWindow("binary", 640,480)
-dst= cv2.resize(binary,dsize=(640,480),interpolation=cv2.INTER_AREA)
-cv2.imshow("binary",dst)
+cv2.namedWindow("binary")
+dst = cv2.resize(binary, dsize=(640, 480), interpolation=cv2.INTER_AREA)
+cv2.imshow("binary", dst)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
